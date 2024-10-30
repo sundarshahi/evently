84bb7441049ed5bdfd9cd8cbae4a4823af1aa2b8
sundarshahi <shahithakurisundar@gmail.com>
2024-10-30 21:28:53 +0545

    chore(service): participant service

---
+++ apps/api/src/services/ParticipantService.ts
@@ -0,0 +1,35 @@
+import { Participant } from "@prisma/client";
+
+import { ParticipantInput } from "../types";
+import { ParticipantRepository } from "../repositories/ParticipantRepository";
+import { EventRepository } from "../repositories/EventRepository";
+
+interface ParticipantService {
+  createParticipant(data: ParticipantInput): Promise<Participant>;
+  getParticipantsByEvent(eventId: string): Promise<Participant[]>;
+}
+
+export const ParticipantServiceFactory = (): ParticipantService => {
+  return {
+    async createParticipant(data: ParticipantInput): Promise<Participant> {
+      const event = await EventRepository.findEventById(data.event_id);
+      if (!event) {
+        throw new Error(`Event with ID ${data.event_id} does not exist`);
+      }
+      const existingParticipant =
+        await ParticipantRepository.findUniqueParticipant(data.email);
+
+      if (existingParticipant) {
+        throw new Error(
+          "A participant with this email already exists for this event."
+        );
+      }
+
+      return ParticipantRepository.createParticipant(data);
+    },
+
+    async getParticipantsByEvent(eventId: string): Promise<Participant[]> {
+      return ParticipantRepository.findParticipantsByEvent(eventId);
+    },
+  };
+};
