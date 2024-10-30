b5a98d6a2b2405ce740a8c7b4276d1047678d0e2
sundarshahi <shahithakurisundar@gmail.com>
2024-10-30 21:28:26 +0545

    chore(repository): participant repository

---
+++ apps/api/src/repositories/ParticipantRepository.ts
@@ -0,0 +1,23 @@
+import { Participant } from "@prisma/client";
+
+import { prisma } from "../config/db";
+import { ParticipantInput } from "../types";
+
+export const ParticipantRepository = {
+  createParticipant: async (data: ParticipantInput): Promise<Participant> =>
+    prisma.participant.create({ data }),
+
+  findUniqueParticipant: async (email: string): Promise<Participant | null> => {
+    return prisma.participant.findUnique({
+      where: {
+        email,
+      },
+    });
+  },
+
+  findParticipantsByEvent: async (eventId: string): Promise<Participant[]> => {
+    return prisma.participant.findMany({
+      where: { event_id: eventId },
+    });
+  },
+};
